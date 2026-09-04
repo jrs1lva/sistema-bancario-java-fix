710eb7e6058e5ba7bfedcd7edc4bcd8ed5452884
jrs1lva <adailtonjunior0604@gmail.com>
2026-09-04 16:30:25 -0300

    fix: ContaCorrenteTest

--- src/br/com/sistemabancariofix/test/ContaCorrenteTest.java
+++ src/br/com/sistemabancariofix/test/ContaCorrenteTest.java
@@ -1,7 +1,6 @@
 package br.com.sistemabancariofix.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
